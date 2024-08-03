82ced52cf84ee4487ce34974c8a6c29ed71ba149
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2024-08-02 22:36:54 -0700

    pyside2 fixes

--- recipes/pyside2/all/conanfile.py
+++ recipes/pyside2/all/conanfile.py
@@ -8,6 +8,7 @@ from conan.tools.scm import Version
 
 import shutil
 import os
+import subprocess
 
 class Pyside2Conanfile(ConanFile):
     name = "pyside2"
@@ -31,14 +32,12 @@ class Pyside2Conanfile(ConanFile):
 
     def build_requirements(self):
         self.tool_requires(f"qt/{self.version}")
-        self.tool_requires("clang/<host_version>")
         self.tool_requires("cpython/<host_version>")
+        self.tool_requires("shiboken2/<host_version>")
 
     def requirements(self):
         self.requires(f"qt/{self.version}")
-        self.requires("libxml2/2.12.7")
-        self.requires("libxslt/1.1.39")
-        self.requires("clang/18.1.0")
+        self.requires(f"shiboken2/{self.version}")
         self.requires("cpython/3.10.14")
 
         self.requires("sqlite3/3.45.2", override=True)
@@ -61,17 +60,6 @@ class Pyside2Conanfile(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def generate(self):
-        env = Environment()
-        env.define_path("CLANG_INSTALL_DIR", self.dependencies["clang"].package_folder)
-        env.vars(self).save_script("clang_env")
-
-        if self.settings.os == "Linux":
-            # On Linux we need to add python's libdirs to the library path so we can find libpython3.12.so.1.0
-            env = Environment()
-            for libdir in self.dependencies["cpython"].cpp_info.libdirs:
-                env.append_path("LD_LIBRARY_PATH", libdir)
-            env.vars(self).save_script("python_env")
-
         ms = VirtualBuildEnv(self)
         ms.generate()
         deps = CMakeDeps(self)
@@ -85,81 +73,21 @@ class Pyside2Conanfile(ConanFile):
     # This method is used to build the source code of the recipe using the desired commands.
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(build_script_folder="sources/pyside2")
         cmake.build()
 
-    @property
-    def _version_suffix(self):
-        v = Version(self.dependencies["cpython"].ref.version)
-        prefix = "cp" if is_msvc(self) else "cpython-"
-        sufffix = ""
-        if is_msvc(self):
-            suffix = "win_amd64"
-        elif self.settings.os == "Linux":
-            suffix = "-x86_64-linux-gnu"
-        elif is_apple_os(self):
-            suffix = "-darwin"
-        #"cpython-310-x86_64-linux-gnu"
-        #"cpython-310-darwin"
-        #"cp310-win_amd64"
-        return f"{prefix}{v.major}{v.minor}{suffix}"
-
-    @property
-    def _abi_suffix(self):
-        res = ""
-        if self.settings.build_type == "Debug":
-            res += "d"
-        return res
-
     def _get_lib_name(self, base_name):
-        if is_msvc(self):
-            if self.settings.build_type == "Debug":
-                lib_ext = "_d"
-            else:
-                lib_ext = ""
-        else:
-            lib_ext = self._abi_suffix
-         #"pyside2.cpython-310-x86_64-linux-gnu"
-         #"pyside2.cpython-310-darwin"
-         #"pyside2.cp310-win_amd64"
-        return f"{base_name}.{self._version_suffix}{lib_ext}"
-
-    def _get_exact_lib_name(self, base_name):
         prefix = "" if self.settings.os == "Windows" else "lib"
-        if self.settings.os == "Windows":
-            extension = "lib"
-        #elif not self.options.shared:
-        #    extension = "a"
-        elif is_apple_os(self):
-            extension = "dylib"
-        else:
-            extension = "so"
-        return f"{prefix}{self._get_lib_name(base_name)}.{extension}"
+        python_bin = os.path.join(
+            self.dependencies["cpython"].cpp_info.bindirs[0], "python3")
+        suffix = subprocess.check_output([python_bin, "-c", 'import importlib.machinery; print(importlib.machinery.EXTENSION_SUFFIXES[0])']).decode("utf-8").strip()
+        return f"{prefix}{base_name}{suffix}"
 
     def package_info(self):
-        self.cpp_info.requires = ["qt::qt", "libxml2::libxml2", "libxslt::libxslt", "cpython::cpython"]
-
-        self.cpp_info.components["libpyside2"].libs = [self._get_exact_lib_name("pyside2")]
+        self.cpp_info.components["libpyside2"].libs = [self._get_lib_name("pyside2")]
         self.cpp_info.components["libpyside2"].libdirs = ["lib"]
         self.cpp_info.components["libpyside2"].includedirs = ["include/PySide2"]
-
-        self.cpp_info.components["libshiboken2"].libs = [self._get_exact_lib_name("shiboken2")]
-        self.cpp_info.components["libshiboken2"].libdirs = ["lib"]
-        self.cpp_info.components["libshiboken2"].includedirs = ["include/shiboken2"]
-
-        self.cpp_info.components["shiboken2"].bins = ["shiboken2"]
-        self.cpp_info.components["shiboken2"].bindirs = ["bin"]
-        self.cpp_info.components["shiboken2"].requires = ["clang::clang"]
-
-        if (self.dependencies['clang'].package_folder):
-            self.buildenv_info.define_path("CLANG_INSTALL_DIR", self.dependencies["clang"].package_folder)
-            self.runenv_info.define_path("CLANG_INSTALL_DIR", self.dependencies["clang"].package_folder)
-
-        if self.settings.os == "Linux":
-            # On Linux we need to add clang libdirs to the library path so we can find libclang
-            for libdir in self.dependencies["clang"].cpp_info.libdirs:
-                self.buildenv_info.append_path("LD_LIBRARY_PATH", libdir)
-                self.runenv_info.append_path("LD_LIBRARY_PATH", libdir)
+        self.cpp_info.requires = ["qt::qt", "shiboken2::libshiboken2", "cpython::cpython"]
 
     def package(self):
         cmake = CMake(self)
