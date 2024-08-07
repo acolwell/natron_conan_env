57155d49d73cc8081c589ab408f57724e6eea989
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2024-08-07 13:22:49 -0700

    Fix typo

--- scripts/natron_conan_env.py
+++ scripts/natron_conan_env.py
@@ -14,7 +14,7 @@ export_recipe_entries = [
 		RecipeInfo("modules/conan-center-index/recipes/qt/5.x.x", "qt", "5.15.14", extra_options=["qt/*:shared=True"]),
 		RecipeInfo("modules/openfx", "openfx", "1.4.0"),
 		RecipeInfo("recipes/clang/all", "clang", "18.1.0"),
-		RecipeInfo("recipes/llvm/all", "llvm", "18.1.0")
+		RecipeInfo("recipes/llvm/all", "llvm", "18.1.0"),
 		RecipeInfo("recipes/openfx-misc/all", "openfx-misc", "master"),
 		RecipeInfo("recipes/openfx-plugin-tools/all", "openfx-plugin-tools", "0.1"),
 		RecipeInfo("recipes/natron/all", "natron", "conan_build"),
